9183d194aaec5df1fbc7245cbe28becde04143a7
krishivpiduri2010 <krishivpiduri337@gmail.com>
2021-04-05 11:49:16 +0530

    Heroku Commit

--- VishnuBirthdaySurprise/urls.py
+++ VishnuBirthdaySurprise/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('',include(urls)),
     path('accounts/',include('django.contrib.auth.urls')),
     path('accounts/',include('accounts.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.ur ls),
 
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
