7839520702e012c2b78c958ec2cbaafa818c4893
krishivpiduri2010 <krishivpiduri337@gmail.com>
2021-04-05 12:17:08 +0530

    Heroku Commit

--- VishnuBirthdaySurprise/settings.py
+++ VishnuBirthdaySurprise/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website',
+    'website'
 ]
 
 MIDDLEWARE = [
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'VishnuBirthdaySurprise.urls'
